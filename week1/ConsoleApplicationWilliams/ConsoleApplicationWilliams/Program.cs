e9d2793eed8a9fc2af8ba2cfddb907c300c0cfa8
Taylor Williams <taylordwilliams90@gmail.com>
2019-01-09 10:53:06 -0700

    week 1 assignment
    v1

---
+++ week1/ConsoleApplicationWilliams/ConsoleApplicationWilliams/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplicationWilliams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"\nHello {name}!");
+
+            int day = DateTime.Now.DayOfYear;
+
+            Console.Write("Day of year: ");
+            Console.WriteLine(day);
+        }
+    }
+}
